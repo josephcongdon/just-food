afb61536b8641abe3b2c38cc428219368547c862
Joseph Congdon <jocongdon@yahoo.com>
2021-12-23 23:51:46 -0600

    new look into anime js

--- index.js
+++ index.js
@@ -3,16 +3,26 @@ import anime from './node_modules/animejs/lib/anime.es.js';
 
 
 
-anime({
-    targets: '.square',
-    translateX: 250
-});
+// anime({
+//     targets: '.square',
+//     translateX: 250
+// });
+
+// anime({
+//     targets: 'div',
+//     translateX: 250,
+//     rotate: '1turn',
+//     backgroundColor: '#FFF',
+//     duration: 800
+// });
 
 anime({
-    targets: 'div',
-    translateX: 250,
-    rotate: '1turn',
-    backgroundColor: '#FFF',
-    duration: 800
+    targets: ['.svg-attributes-demo polygon', 'feTurbulence', 'feDisplacementMap'],
+    points: '64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96',
+    baseFrequency: 0,
+    scale: 1,
+    loop: true,
+    direction: 'alternate',
+    easing: 'easeInOutExpo'
 });
 
---
+++ js/just-food.js
@@ -0,0 +1,7 @@
+$.ajax({
+    url: 'https://api.spoonacular.com/food/products/search',
+    type: "GET",
+    data:{
+        APP
+    }
+})
